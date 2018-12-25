9356ae9cb0aa5d86275b20c41fc8f94c3f7de69f
cstyl <s1887443@ed.ac.uk>
2018-12-25 19:34:53 +0000

    comments added in performance script

--- scripts/performance/performance.sh
+++ scripts/performance/performance.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-# set -o allexport
+: '
+Description:
+	Script to run and submit performance tests on the back-end of CIRRUS 
+	using the performance.pbs script located in ./res/pbs/.
+	Parses the command line arguments and determines which performance test is to be performed.
+	Two options of tests are available:
+		- Single node performance test
+		- Multiple nodes performance test
+	Note that it assumes correct input arguments and does not check for their correctness.
+'
+: '
+Inputs:
+	Command line arguments below:
+		- EXEC = the name of the executable to be executed
+		- REPS = number of repetitions each experiment is performed
+		- ITERS = number of maximum allowed iterations of the main loop of parallel code
+		- TEST = name of the test to be performed i.e singlenode or mulnodes
+		- NODES = number of nodes required by the test
+		- TIME = requested time to commit the resources on CIRRUS
+		- RES = reservation name if exists
+'
+: '
+Outputs:
+	- logfile.txt: Contains cirrus logs.
+	- perf.csv: Contains the time measurements from each test.
+Both are located in ${RES_DIR}
+'
 
 printf "Parsing command line arguments...\t"
 
@@ -55,9 +81,11 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 if [ "$EXEC" = "bin/image_overlap" ]
 then
 	CASE=overlap
+	make bin/image_overlap DEFINE=-DOVERLAP DEFINE+=-DTIME
 elif [ "$EXEC" = "bin/image_no_overlap" ]
 then
 	CASE=no_overlap
+	make bin/image_no_overlap DEFINE=-DTIME
 fi
 
 printf "Done.\nSetting up ${CASE}_${TEST}${NODES} test...\t"
@@ -98,7 +126,7 @@ mkdir -p ${RES_DIR}
 
 TEMP_FILE=${OUT_DIR}/${NAME}_out.txt
 TEMP_RES_FILE=${OUT_DIR}/${NAME}_temp_res.txt
-PERF_FILE=${RES_DIR}/${NAME}_perf.txt
+PERF_FILE=${RES_DIR}/${NAME}_perf.csv
 LOG_FILE==${RES_DIR}/${NAME}_log.txt
 
 printf "Done.\nExecuting test on backend nodes...\n"
